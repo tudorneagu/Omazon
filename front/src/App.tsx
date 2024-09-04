29a7c38d3692320ad74a9a4e1e9248a4acad8a0d
tudorneagu <neagu.t@gmail.com>
2024-09-04 13:14:14 +0200

    back prisma regenarated

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,40 +2,35 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./pages/Home";
 import Footer from "./components/Footer/Footer";
-import { AddProductProvider } from "./contexts/AddProductContext";
-import { AuthProvider } from "./contexts/AuthContext";
+
 import CategoryPage from "./pages/CategoryPage";
 import ProductPage from "./pages/ProductPage";
-import { CartProvider } from "./contexts/CartContext";
+
 import CartPage from "./pages/CartPage";
+import AddProduct from "./components/ui/Modals/AddProduct";
+import { useContext } from "react";
+import { AddProductContext } from "./contexts/AddProductContext";
 
 function App() {
+	const { isModalOpen } = useContext(AddProductContext);
 	return (
 		<BrowserRouter>
-			<AuthProvider>
-				<AddProductProvider>
-					<CartProvider>
-						<Header />
-						<main className="flex justify-around">
-							<div className="max-w-[1600px] w-full">
-								<Routes>
-									<Route path="/" element={<Home />} />
-									<Route
-										path="/category/:categoryTitle/products"
-										element={<CategoryPage />}
-									/>
-									<Route
-										path="/product/:productTitle"
-										element={<ProductPage />}
-									/>
-									<Route path="/cart" element={<CartPage />} />
-								</Routes>
-							</div>
-						</main>
-						<Footer />
-					</CartProvider>
-				</AddProductProvider>
-			</AuthProvider>
+			<Header />
+			<main className="flex justify-around">
+				{isModalOpen && <AddProduct />}
+				<div className="max-w-[1600px] w-full">
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route
+							path="/category/:categoryTitle/products"
+							element={<CategoryPage />}
+						/>
+						<Route path="/product/:productTitle" element={<ProductPage />} />
+						<Route path="/cart" element={<CartPage />} />
+					</Routes>
+				</div>
+			</main>
+			<Footer />
 		</BrowserRouter>
 	);
 }
--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,12 +1,21 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ProductProvider } from "./contexts/ProductContext";
+import { ProductProvider } from "./contexts/ProductContext.tsx";
+import { AddProductProvider } from "./contexts/AddProductContext";
+import { AuthProvider } from "./contexts/AuthContext";
+import { CartProvider } from "./contexts/CartContext";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
 	<>
 		<ProductProvider>
-			<App />
+			<AuthProvider>
+				<AddProductProvider>
+					<CartProvider>
+						<App />
+					</CartProvider>
+				</AddProductProvider>
+			</AuthProvider>
 		</ProductProvider>
 	</>,
 );
