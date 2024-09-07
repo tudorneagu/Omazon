b93722d8a7a2cc06cf514938e66483d182eb67a6
tudorneagu <neagu.t@gmail.com>
2024-09-07 21:21:33 +0200

    user registration done

--- front/src/contexts/AuthContext.tsx
+++ front/src/contexts/AuthContext.tsx
@@ -1,83 +1,127 @@
-import { createContext, useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import authService from "../services/authService";
+import { createContext, useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import authService from '../services/authService';
 const AuthContext = createContext();
 
 function AuthProvider({ children }: { children: React.ReactNode }) {
-	const [loginForm, setLoginForm] = useState(true);
-	const [loged, setLoged] = useState(false);
-	const [authData, setAuthData] = useState(null);
-	const [error, setError] = useState("");
+  const [loginForm, setLoginForm] = useState(true);
+  const [loged, setLoged] = useState(false);
+  const [authData, setAuthData] = useState(null);
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState('');
 
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		const checkAuth = async () => {
-			try {
-				const response = await authService.checkAuth();
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await authService.checkAuth();
 
-				if (response) {
-					const storedAuthData = localStorage.getItem("authData");
-					if (storedAuthData) setAuthData(JSON.parse(storedAuthData));
+        if (response) {
+          const storedAuthData = localStorage.getItem('authData');
+          if (storedAuthData) setAuthData(JSON.parse(storedAuthData));
 
-					setLoged(true);
-					console.log(authData);
-				}
-			} catch (error) {
-				console.log("Authentication check failed:", error);
-				setAuthData(null);
-				setLoged(false);
-				localStorage.removeItem("authData");
-			}
-		};
-		checkAuth();
-	}, []);
+          setLoged(true);
+        }
+      } catch (error) {
+        console.log('Authentication check failed:', error);
+        setAuthData(null);
+        setLoged(false);
+        localStorage.removeItem('authData');
+      }
+    };
+    checkAuth();
+  }, []);
 
-	const loginUser = async (email, password) => {
-		setError(null);
-		try {
-			const userData = await authService.login(email, password);
-			console.log("User data received:", userData);
-			setAuthData(userData);
-			console.log(userData);
-			localStorage.setItem("authData", JSON.stringify(userData));
+  const registerUser = async (formData) => {
+    const {
+      firstname,
+      lastname,
+      email,
+      password,
+      passwordConfirm,
+      account_type,
+    } = formData;
+    setError(null);
+    setSuccess(null);
+    try {
+      if (password.length < 6) {
+        setError('Le mot de passe doit comporter au moins 6 caractères.');
+        return { field: 'password' };
+      }
+      if (password !== passwordConfirm) {
+        setError('Les mots de passe ne correspondent pas.');
+        return { field: 'password' };
+      }
 
-			setLoginForm(false);
-			setLoged(true);
-			localStorage;
-			navigate("/");
-		} catch (error) {
-			setError("Email or password incorrect. Please try again.");
-		}
-	};
+      const newUserData = await authService.register({
+        firstname,
+        lastname,
+        email,
+        password,
+        account_type,
+      });
 
-	const logoutUser = async () => {
-		setError(null);
-		try {
-			await authService.logout();
-			setAuthData(null);
-			localStorage.removeItem("userData");
-			setLoged(false);
-			navigate("/");
-		} catch (error) {
-			setError("Logout failed. Please try again.");
-		}
-	};
+      setAuthData(newUserData);
+      setLoged(true);
+      localStorage.setItem('authData', JSON.stringify(newUserData));
+      setSuccess('User created successfully!');
+      navigate('/');
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        setError('Un compte avec cet email existe déjà..');
+        return { field: 'email' };
+      } else {
+        setError('Error during the registration process. Please try again.');
+      }
+    }
+  };
 
-	return (
-		<AuthContext.Provider
-			value={{
-				authData,
-				loginForm,
-				loginUser,
-				logoutUser,
-				loged,
-				error,
-			}}
-		>
-			{children}
-		</AuthContext.Provider>
-	);
+  const loginUser = async (email, password) => {
+    setError(null);
+    try {
+      const userData = await authService.login(email, password);
+      console.log('User data received:', userData);
+      setAuthData(userData);
+      console.log(userData);
+      localStorage.setItem('authData', JSON.stringify(userData));
+
+      setLoginForm(false);
+      setLoged(true);
+      navigate('/');
+    } catch (error) {
+      setError('Email or password incorrect. Please try again.');
+    }
+  };
+
+  const logoutUser = async () => {
+    setError(null);
+    try {
+      await authService.logout();
+      setAuthData(null);
+      localStorage.removeItem('authData');
+      setLoged(false);
+      navigate('/');
+    } catch (error) {
+      setError('Logout failed. Please try again.');
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        registerUser,
+        loginUser,
+        logoutUser,
+        loged,
+        authData,
+        error,
+        success,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 export { AuthProvider, AuthContext };
